4319c8db91d1b7ad056fa0c1eb83de4bd79b6951
Salva Pérez <txaxati2018@gmail.com>
2021-09-27 12:24:02 +0200

    Comentando código

--- Programación/Prueba inicial/src/Main.java
+++ Programación/Prueba inicial/src/Main.java
@@ -2,75 +2,78 @@ import java.util.Scanner;
 
 public class Main {
 	
-	static Scanner entradaTexto = new Scanner(System.in);
+	static Scanner entradaTexto = new Scanner(System.in);												// Creamos un nuevo Scanner del teclado estático
 	
 	public static void main(String[] args) {
-		Edificio edificio = new Edificio();
-		int opcion = 0;
+		Edificio edificio = new Edificio();																// Creamos un nuevo edificio
+		int opcion = 0;																					// Esta variable almacenará la última opción elegida por el usuario
 		
 		do {
-			mostrarMenu();
+			mostrarMenu();																				// Mostramos el menú
 			
-			String entrada;
+			String entrada;																				// Esta variable almacenará la respuesta del usuario
 			
 			do {
-				entrada = leerCadena("Seleccione número de función");
+				entrada = leerCadena("Seleccione número de función");									// Pedimos un número de función mientras no sea correcta la respuesta
 			}
 			while (!entrada.matches("[+-]?\\d*(\\.\\d+)?"));
 			
-			opcion = Integer.parseInt(entrada);
+			opcion = Integer.parseInt(entrada);															// Convertimos a entero u lo añadimos a opcion
 			
 			try {
-				switch (opcion) {
+				switch (opcion) {																		// Evaluamos la opción y lanzamos la función correspondiente
 					case 1: {
-						edificio.agregarPlanta();
+						edificio.agregarPlanta();																// Agrega una planta
 						break;
 					}
 					case 2: {
-						edificio.agregarZonaAPlanta();
+						edificio.agregarZonaAPlanta();															// Agrega una zona a una planta
 						break;
 					}
 					case 3: {
-						edificio.calcularArea();
+						edificio.calcularArea();																// Calcula el área del edificio
 						break;
 					}
 					case 4: {
-						edificio.calcularAreaPlanta();
-						break;
+						edificio.calcularAreaPlanta();															// Calcula el área de una planta
+						break;	
 					}
 					case 5: {
-						edificio.calcularAreaZona();
+						edificio.calcularAreaZona();															// Calcula el área de una zona
 						break;
 					}
 					case 6: {
-						edificio.exportarPlantasAJson();
+						edificio.exportarPlantasAJson();														// Exporta las plantas a Json
 						break;
 					}
 					case 7: {
-						edificio.exportarPlantasAarchivo();
+						edificio.exportarPlantasAarchivo();														// Exporta plantas a archivo de texto plano
 						break;
 					}
-					case 8: {
-						edificio.mostrarPlantas();
+					case 8: {	
+						edificio.mostrarPlantas();																// Muestra las plantas
 						break;
 					}
 					case 9: {
-						System.out.println("Cerrando ...");
-						entradaTexto.close();
+						System.out.println("Cerrando ...");														// Indicamos que se va a cerrar el programa
+						entradaTexto.close();																	// Cerramos el scanner
 						break;
 					}
 					default: {
-						System.out.println("Opción incorrecta");
+						System.out.println("Opción incorrecta");												// Indicamos que la opción es incorrecta en caso de que el número no corresponda con un número de función váida
 					}
 				}
 			}
 			catch(Exception e) {
-				System.out.println("Error: " + e);
+				System.out.println("Error: " + e);																// En caso de excepción la capturamos y la mostramos
 			}
 		}
-		while (opcion != 9);
+		while (opcion != 9);																					// Mostramos el menú mientras no se quiera salir
 	}
 	
+	/**
+	 * Muestra el menú de la aplicación
+	 */
 	static void mostrarMenu() {
 		System.out.println(
 							"\n" +
@@ -88,14 +91,18 @@ public class Main {
 							
 		);
 	}
-	
+	/**
+	 * Pide una cadena, la valida y la devuelve
+	 * @param mensaje
+	 * @return
+	 */
 	static String leerCadena(String mensaje) {
-		String resultado;
-		do {
-			System.out.print(mensaje + " >> ");
-			resultado = entradaTexto.nextLine().trim();
+		String resultado;																// Esta variable almacenará el resultado
+		do {	
+			System.out.print(mensaje + " >> ");												// Mostramos el mensaje
+			resultado = entradaTexto.nextLine().trim();										// Pedimos una cadena
 		}
-		while (resultado.length() == 0);
-		return resultado;
+		while (resultado.length() == 0);												// Pedimos mientras no se inserte nada
+		return resultado;																// Retornamos el resultado
 	}
 }
